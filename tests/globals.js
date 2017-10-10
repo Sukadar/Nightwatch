d8a1df1e3adfd99a33230c51176935232d9e3959
sukadar <sukada.ramakrishna@socialmedialink.com>
2017-10-10 11:22:40 -0400

    first

--- tests/globals.js
+++ tests/globals.js
@@ -8,11 +8,17 @@ module.exports = {
   postalcode:'K1A0B1',
   dob:'10-10-1980',
   lcn:'123456789012',
-  adminemail:'sukar@yopmail.com',
+  adminemail:'sureg@yopmail.com',
   adminpassword:'General1234#',
   surveyname:'survey',
+  activityname:'Test Activity',
+  bvstagingclnttoken:'caEg1TBXTKhIcmnk623URKYPzAYlrtoLG2LkHaqD4neTk',
+  bvstagingenkey:'6VgQ5XhdqOaVnj2FlvjqjMTxc',
+  bvstagingprodid:'Product10',
  urls:{
     memberurl:'https://final88.staging.socialmedialink.com/home#/',
-    adminurl:'https://admin.staging.socialmedialink.com/admins/sign_in',
+    adminurl:'https://admin.staging.socialmedialink.com',
+    adminyoutubeurl:'https://www.youtube.com/watch?v=cmSbXsFE3l8',
  }
+
 };
